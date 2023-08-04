18f84801a84559370017bcd0fe62cb7b75170d8e
s3782003 <62494764+s3782003@users.noreply.github.com>
2023-08-04 17:45:04 +1000

    Added comments to Coordinate.cpp

--- assignment2/Coordinate.cpp
+++ assignment2/Coordinate.cpp
@@ -1,6 +1,7 @@
 #include "Coordinate.h"
 #include "Tile.h"
 
+//Establishing key variables to be reference on the board.
 Coordinate::Coordinate(int row, int col) {
   this->rowCoordinate = row;
   this->colCoordinate = col;
@@ -12,22 +13,22 @@ Coordinate::~Coordinate() {
   delete this->playedTile; // LinkedList take care of this?
 }
 
-
+// Gets row co-ordinate.
 int Coordinate::getRowCoordinate() {
   return this->rowCoordinate;
 }
 
-
+// Gets column co-ordinate.
 int Coordinate::getColCoordinate() {
   return this->colCoordinate;
 }
 
-
+// Identifies played tile.
 void Coordinate::setPlayedTile(Tile* tile) {
   this->playedTile = tile;
 }
 
-
+// Returns played tile.
 Tile* Coordinate::getPlayedTile() {
   return this->playedTile;
 }
